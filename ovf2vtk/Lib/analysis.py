e5c0b30f7064c9ff675a60d243b96838aa6d3c04
Hans <fangohr@soton.ac.uk>
2015-10-22 15:47:30 +0100

    pep8-improvement for magnitude()

--- ovf2vtk/Lib/analysis.py
+++ ovf2vtk/Lib/analysis.py
@@ -34,13 +34,14 @@ except ImportError:
 __version__ = "$Revision: 1.3 $"
 
 
-def magnitude( d ):
+def magnitude(d):
     """expects d to be a 1xN array,
     returns the magnitude (standard 2-Norm)"""
-    #square entries:                  d*d
-    #sum over 3-components (axis 1):  add.reduce( d*d, 1)
-    #take square root                 sqrt( add.reduce ( d*d, 1 ) )
-    return Numeric.sqrt( Numeric.add.reduce( d*d, 1 ) )
+    # square entries:                  d*d
+    # sum over 3-components (axis 1):  add.reduce( d*d, 1)
+    # take square root                 sqrt( add.reduce ( d*d, 1 ) )
+    return Numeric.sqrt(Numeric.add.reduce(d * d, 1))
+
 
 def convert_flat_fortran_to_3dmatrix( vf, Nx, Ny, Nz ):
     return Numeric.resize( vf, (Nz,Ny,Nx,3)) #this is in Fortran order
