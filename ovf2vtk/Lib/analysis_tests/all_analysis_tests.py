a145e7811730c21b0bd1cd20795846d25a7dcf7c
hwilson94 <hw1g13@soton.ac.uk>
2016-03-24 19:42:10 +0000

    removed old code

--- ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
+++ ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
@@ -28,42 +28,6 @@ def test_magnitude_n():
     # code works?
     assert exp == act
 
-    x = np.array([[1, 2, 3.]])
-    # requirement for magnitude:
-    assert x.shape == (1, 3)
-
-    # compute result we expect
-    y1 = math.sqrt(1 ** 2 + 2 ** 2 + 3 ** 2)
-
-    # compute actual result from analysis.magnitude
-    y2 = analysis.magnitude(x)
-    assert y1 == float(y2)
-
-    # n = 5
-    x = np.array([[1, 2, 3., 0.1, 0]])
-    # requirement for magnitude:
-    assert x.shape == (1, 5)
-
-    # compute result we expect
-    y1 = math.sqrt(1 ** 2 + 2 ** 2 + 3 ** 2 + 0.1 ** 2 + 0 ** 2)
-
-    # compute actual result from analysis.magnitude
-    y2 = analysis.magnitude(x)
-    assert y1 == float(y2)
-
-    # n = 3, with negative entries
-
-    x = np.array([[-1, -2, 3.]])
-    # requirement for magnitude:
-    assert x.shape == (1, 3)
-
-    # compute result we expect
-    y1 = math.sqrt((-1) ** 2 + (-2) ** 2 + 3. ** 2)
-
-    # compute actual result from analysis.magnitude
-    y2 = analysis.magnitude(x)
-    assert y1 == float(y2)
-
 
 def test_magnitude_bordercase():
     # n = 1
