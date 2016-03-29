9e60e5c89fa78b103f76448e84b2eb4fba658dc4
hwilson94 <hw1g13@soton.ac.uk>
2016-03-29 16:21:20 +0100

    removed old duplicate code for 'test_convert_c_to_fortan' function.

--- ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
+++ ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
@@ -143,45 +143,6 @@ def test_convert_c_to_fortran():
                     for w in range(3):
                         assert a1[z, y, x, w] == act[x, y, z, w]
 
-    # shape (3, 1, 3)
-
-    a = np.array([[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]])
-    # compute actual result
-    Xact = analysis.convert_C_to_fortran(a)
-    # compute expected result
-    Xexp = analysis.convert_fortran_to_C(a)
-    # ensure actual and expected are identical
-    assert Xact.all() == Xexp.all()
-
-    # check shape returns as expected
-
-    # shape (3, 4, 5)
-
-    a = np.ones((3, 4, 5))
-    # compute actual result
-    Xact = analysis.convert_C_to_fortran(a)
-    # check its shape
-    assert Xact.shape == (long(5), long(4), long(3))
-    # compute expected result
-    Xexp = analysis.convert_fortran_to_C(a)
-    # ensure actual and expected are identical
-    assert Xact.all() == Xexp.all()
-
-    # shape (3, 4, 5)
-
-    a = np.ones((1, 6, 4, 5))
-    # compute actual result
-    Xact = analysis.convert_C_to_fortran(a)
-    # check its shape
-    assert Xact.shape == (long(4), long(6), long(1), long(5))
-    # compute expected result
-    Xexp = analysis.convert_fortran_to_C(a)
-    # ensure actual and expected are identical
-    assert Xact.all() == Xexp.all()
-
-
-"""fortanindex"""
-
 
 def test_fortranindex():
 
