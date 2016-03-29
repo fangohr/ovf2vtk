ab38b5e27bf7324707060e366196c16e2bcedb07
hwilson94 <hw1g13@soton.ac.uk>
2016-03-29 14:02:07 +0100

    deleted old, duplicate code in 'test_convert_fortran_3dmatrix_to_flat'.

--- ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
+++ ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
@@ -75,47 +75,6 @@ def test_convert_fortran_3dmatrix_to_flat():
         # test actual result is 1D with same length as expected
         assert (long(length),) == Mflat.shape
 
-    M = np.ones((1, 2))
-    # compute actual result from tested function
-    Mrav = analysis.convert_fortran_3dmatrix_to_flat(M)
-    # test computed result is a numpy array
-    assert isinstance(Mrav, np.ndarray)
-    # compute length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # test actual result is 1D with same length as expected
-    assert (long(x),) == Mrav.shape
-
-    # shape = (1, 2, 3)
-
-    M = np.ones((1, 2, 3))
-    # compute actual result from tested function
-    Mrav = analysis.convert_fortran_3dmatrix_to_flat(M)
-    # test computed result is a numpy array
-    assert isinstance(Mrav, np.ndarray)
-    # compute length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # test actual result is 1D with same length as expected
-    assert (long(x),) == Mrav.shape
-
-    # test shapes between 4D and 10D
-
-    for S in range(4, 11, 1):
-        M = np.ones(tuple(range(1, S+1, 1)))
-        # compute actual result from tested function
-        Mrav = analysis.convert_fortran_3dmatrix_to_flat(M)
-        # test computed result is a numpy array
-        assert isinstance(Mrav, np.ndarray)
-        # compute length of flattened array
-        x = 1
-        for i in M.shape:
-            x = x * i
-        # test actual result is 1D with same length as expected
-        assert (long(x),) == Mrav.shape
-
 
 def test_f3D2f_bordercase():
 
