5933e17237bf743d563c6546c43e64f406489b0f
hwilson94 <hw1g13@soton.ac.uk>
2016-03-29 14:48:33 +0100

    deleted old, duplicate code for 'test_convert_fortran_3dmatrix_to_flat_vector' and its bordercases.

--- ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
+++ ovf2vtk/Lib/analysis_tests/all_analysis_tests.py
@@ -85,139 +85,19 @@ def test_convert_fortran_3dmatrix_to_flat_vector():
     for i in range(5):
         M = np.ndarray((Nzs[i], Nys[i], Nxs[i], 3))
         # compute expected length of flattened array
-        x = 1
+        length = 1
         for i in M.shape:
-            x = x * i
+            length = length * i
         # compute actual length of flattened array
-        Mflatv = M.ravel()
+        Mlength = M.ravel()
         # compare expected vs actual
-        assert int(x) == len(Mflatv)
+        assert int(length) == len(Mlength)
         # compute actual returned function
         flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
         # check if returned value is a numpy array
         assert isinstance(flatv, np.ndarray)
         # assert returned array of required shape
-        assert flatv.shape == (long(x)/3, long(3))
-
-
-    M = np.ones((2, 3))
-    # compute expected length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # compute actual length of flattened array
-    Mrav = M.ravel()
-    # compare expected vs actual
-    assert int(x) == len(Mrav)
-    # compute actual returned function
-    flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
-    # check if returned value is a numpy array
-    assert isinstance(flatv, np.ndarray)
-    # assert returned array of required shape
-    assert flatv.shape == (long(x)/3, long(3))
-
-    # shape = (4, 7)
-
-    M = np.ones((4, 7))
-    # compute expected length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # compute actual length of flattened array
-    Mrav = M.ravel()
-    # compare expected vs actual
-    assert int(x) == len(Mrav)
-    # compute actual returned function
-    flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
-    # check if returned value is a numpy array
-    assert isinstance(flatv, np.ndarray)
-    # assert returned array of required shape
-    assert flatv.shape == (long(x)/3, long(3))
-
-    # shape = (20, 35) with negative entries
-
-    M = np.ones((20, 35))
-    # compute expected length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # compute actual length of flattened array
-    Mrav = M.ravel()
-    # compare expected vs actual
-    assert int(x) == len(Mrav)
-
-    # compute actual returned function
-    flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
-    # check if returned value is a numpy array
-    assert isinstance(flatv, np.ndarray)
-    # assert returned array of required shape
-    assert flatv.shape == (long(x)/3, long(3))
-
-
-def test_f3Dm2fv_bordercase():
-
-    # shape = (1, 1)
-
-    M = np.ones((1, 1))
-    # compute expected length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # compute actual length of flattened array
-    Mrav = M.ravel()
-    # compare expected vs actual
-    assert int(x) == len(Mrav)
-
-    # compute actual returned function
-    flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
-    # check if returned value is a numpy array
-    assert isinstance(flatv, np.ndarray)
-    # assert returned array of required shape
-    assert int(x) < 3
-    assert flatv.shape == (long(x)/3,)
-
-    # shape = (0)
-
-    M = np.ones((0))
-    # compute expected length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # compute actual length of flattened array
-    Mrav = M.ravel()
-    # compare expected vs actual
-    assert int(x) == len(Mrav)
-
-    # compute actual returned function
-    flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
-    # check if returned value is a numpy array
-    assert isinstance(flatv, np.ndarray)
-    # assert returned array of required shape
-    assert int(x) < 3
-    assert flatv.shape == (long(x)/3, long(3))
-
-    # shape = (1, 2):
-
-    M = np.ones((1, 2))
-    # compute expected length of flattened array
-    x = 1
-    for i in M.shape:
-        x = x * i
-    # compute actual length of flattened array
-    Mrav = M.ravel()
-    # compare expected vs actual
-    assert int(x) == len(Mrav)
-
-    # compute actual returned function
-    flatv = analysis.convert_fortran_3dmatrix_to_flat_vector(M)
-    # check if returned value is a numpy array
-    assert isinstance(flatv, np.ndarray)
-    # assert returned array of required shape
-    assert int(x) < 3
-    assert flatv.shape == (long(x)/3,)
-
-
-"""fortran_to_c"""
+        assert flatv.shape == (long(length)/3, long(3))
 
 
 def test_f2c():
