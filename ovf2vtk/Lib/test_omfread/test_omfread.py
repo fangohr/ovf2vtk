01322aded9af12251049a933ec3049d4ae429039
hwilson94 <hw1g13@soton.ac.uk>
2016-04-12 16:42:00 +0100

    added 'lines variable' in 'test_parse_for_keywords'

--- ovf2vtk/Lib/test_omfread/test_omfread.py
+++ ovf2vtk/Lib/test_omfread/test_omfread.py
@@ -10,24 +10,30 @@ one place. By Harry Wilson. Last updated 09/04/16"""
 
 # *************************** Global Variables ***************************** #
 
-keywords=["Title:",
-          "Desc: Applied field (T):",
-          "Desc: Iteration:",
-          "meshtype:", "meshunit:",
-          "xbase:", "ybase:", "zbase:",
-          "xstepsize:", "ystepsize:", "zstepsize:",
-          "xnodes:", "ynodes:", "znodes:",
-          "xmin:","ymin:","zmin:","xmax:","ymax:","zmax:",
-          "boundary:","valueunit:","valuemultiplier:",
-          "ValueRangeMinMag:","ValueRangeMaxMag:",
-          "End: Head:", "Begin: Data:", "OOMMF:", "Segment count:","Begin: Segme"]
+keywords = ["Title:",
+            "Desc: Applied field (T):",
+            "Desc: Iteration:",
+            "meshtype:", "meshunit:",
+            "xbase:", "ybase:", "zbase:",
+            "xstepsize:", "ystepsize:", "zstepsize:",
+            "xnodes:", "ynodes:", "znodes:",
+            "xmin:", "ymin:", "zmin:", "xmax:", "ymax:", "zmax:",
+            "boundary:", "valueunit:", "valuemultiplier:",
+            "ValueRangeMinMag:", "ValueRangeMaxMag:",
+            "End: Head:", "Begin: Data:", "OOMMF:", "Segment count:",
+            "Begin: Segme"]
 
-filenames = ['C:\Users\Harry\Documents\Examples\cantedvortex.omf', 
+filenames = ['C:\Users\Harry\Documents\Examples\cantedvortex.omf',
              'C:\Users\Harry\Documents\Examples\ellipsoidwrap.omf',
              'C:\Users\Harry\Documents\Examples\h2hleftedge.ohf',
              'C:\Users\Harry\Documents\Examples\yoyoleftedge.ohf']
 
+
 def test_parse_for_keywords():
+    lines = ['# xmax: 4\n', '# xmin: 5\n', 'xbase: 5\n', 'ybase: 3/n', 
+             '# boundary: 10', '# valueunit: 8', 'znodes: 8', 'meshunit: 4'
+             '# ymax:   34', '# valuemultiplier:  475/n']
+    
 
 
 def test_analyze():
