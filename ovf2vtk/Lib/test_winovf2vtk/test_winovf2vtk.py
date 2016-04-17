52717b50df3f5dcd3a7c833f6b222c96a15771de
hwilson94 <hw1g13@soton.ac.uk>
2016-04-17 01:40:20 +0100

    created initial tests for for posiible key and parameter combinations in 'test_winovf2vtk_keys_two_parameters'.

--- ovf2vtk/Lib/test_winovf2vtk/test_winovf2vtk.py
+++ ovf2vtk/Lib/test_winovf2vtk/test_winovf2vtk.py
@@ -49,6 +49,16 @@ cells_filled = ['100.00', '57.29', '100.00', '99.60', '100.00', '100.00',
 scale = ['1.000000', '1.000000', '1.000000', '1.000000', '349370.681891',
          '270851.375948', '1.500000', '21.954498', '1.000000']
 
+# nodes for each corresponding infile
+nodes = [(32, 32, 32), (24, 8, 4), (15, 15, 15), (50, 100, 1),
+         (160, 40, 4), (500, 6, 2), (5, 3, 1), (3, 2, 1), (1, 1, 13)]
+
+# floatsize for the first six infiles (binary files)
+floatsizes = [4, 4, 4, 4, 8, 8]
+
+# list giving startbyte values to corresponding file in 'infiles'
+bytes = [874, 850, 797, 518, 874, 754, 488, 505, 468]
+
 # *********************************** Tests *********************************
 
 
@@ -197,7 +207,97 @@ VtkData.__init__.skipping:
 
 
 def test_winovf2vtk_keys_two_parameters():
-     """test that the expected output is displayed when the command line
+    """test that the expected output is displayed when the command line
     includes a key and two parameters: the infile and outfile"""
-    
-    
+    for i in range(len(keys)):
+        for j in range(len(infiles)):
+            # actual result
+            command = "python.exe C:\Users\Harry\Anaconda\Scripts\
+\winovf2vtk.py" + " {} {} {}".format(keys[i], infiles[j], outfiles[j])
+            p = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                 stderr=subprocess.STDOUT)
+            doc = p.stdout.readlines()
+            # remove '\r\n' characters
+            new_doc = []
+            for k in range(len(doc)):
+                line = doc[k].strip('\r\n')
+                new_doc.append(line)
+            # compute expected results
+
+            # '-V file.omf file.vtk'
+            if i < 2:
+                exp = ["This is version {}.".format(ovf2vtk.__version__)]
+                assert exp == new_doc
+                assert "This is version {}.".format(ovf2vtk.__version__) in \
+                    str(doc)
+
+            # '-v asciifile.omf asciifile.vtk'
+            if 1 < i < 4 and j < 6:
+                # '-v asciifile.omf asciifile.vtk'
+                options = {keys[i]: None}
+                exp = "running in verbose mode\n" + 70 * "-" + \
+                    "\novf2vtk --- converting ovf files to vtk files" + \
+                    "\n" + """Hans Fangohr, Richard Boardman, University of \
+Southampton\n""" + 70 * "-" + "\n" + \
+                    """infile  =  {}
+outfile =  {}
+additions=  [('{}', '')]
+options =  {}
+datascale= 0.0
+posscale= 0.0
+Number of cells (Nx={},Ny={},Nz={})
+Expect floats of length {} bytes.
+Expect to find data in file {}  at position {} .
+verification_tag is okay (=> reading byte order correctly)
+({}% of {} cells filled)
+Will scale data down by {}
+saving file ({})
+finished conversion (execution time
+VtkData.__init__.skipping:
+\tstriping header string to a length =255""".format(infiles[j], outfiles[j],
+                                                    keys[i], options,
+                                                    nodes[j][0], nodes[j][1],
+                                                    nodes[j][2], floatsizes[j],
+                                                    infiles[j], bytes[j],
+                                                    cells_filled[j], cells[j],
+                                                    scale[j], outfiles[j])
+                exp = exp.splitlines()
+                # can't predict execution time
+                new_doc[-3] = new_doc[-3][:35]
+                assert exp == new_doc
+
+            # '-v asciifile.omf asciifile.vtk'
+            if i < 1 < 4 and j > 6:
+                options = {keys[i]: None}
+                exp = "running in verbose mode\n" + 70 * "-" + \
+                    "\novf2vtk --- converting ovf files to vtk files" + \
+                    "\n" + """Hans Fangohr, Richard Boardman, University of \
+Southampton\n""" + 70 * "-" + "\n" + \
+                    """infile  =  {}
+outfile =  {}
+additions=  [('{}', '')]
+options =  {}
+datascale= 0.0
+posscale= 0.0
+Number of cells (Nx={},Ny={},Nz={})
+Hint: Reading ASCII-OOMMF file is slow (that could be changed) and the files \
+are large. Why not save data as binary?
+({}% of {} cells filled)
+Will scale data down by {}
+saving file ({})
+finished conversion (execution time
+VtkData.__init__.skipping:
+\tstriping header string to a length =255""".format(infiles[j], outfiles[j],
+                                                    keys[i], options,
+                                                    nodes[j][0], nodes[j][1],
+                                                    nodes[j][2],
+                                                    cells_filled[j], cells[j],
+                                                    scale[j], outfiles[j])
+                exp = exp.splitlines()
+                # can't predict execution time
+                new_doc[-3] = new_doc[-3][:35]
+                assert exp == new_doc
+            if 3 < i < 6:
+                exp = winovf2vtk.__doc__ + "\n"
+                exp = exp.splitlines()
+                assert exp == new_doc
