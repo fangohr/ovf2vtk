b66d859062c016aa69a71fd3f28ab5ce1dc66dd8
hwilson94 <hw1g13@soton.ac.uk>
2016-04-23 21:32:11 +0100

    Try changing str type to support buffer interface. Working towards compatibilty for Python 3.4 and Python 3.5.

--- ovf2vtk/Lib/omfread_new.py
+++ ovf2vtk/Lib/omfread_new.py
@@ -87,7 +87,7 @@ def what_data(filename, verbose=0):
         lines += 1
         if not line:               # until eof
             f.close()
-            print(encode(b'***Reached end of file before encountering data', 'hex'))
+            print(bytearray(b'***Reached end of file before encountering data', 'utf-8'))
             print("   Cowardly stopping here")
             print("   Some debug info:")
             print("   Have read {} lines and".format(lines))
