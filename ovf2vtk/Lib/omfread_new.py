bbaaf71cc0a6778e4b3bc2a8d41786fe2c9b3a60
hwilson94 <hw1g13@soton.ac.uk>
2016-04-24 00:25:25 +0100

    try to get str to support buffer interface.

--- ovf2vtk/Lib/omfread_new.py
+++ ovf2vtk/Lib/omfread_new.py
@@ -86,8 +86,7 @@ def what_data(filename, verbose=0):
         bytes += len(line)
         lines += 1
         if not line:               # until eof
-            f.close()
-            print(bytearray(b'***Reached end of file before encountering data', 'ascii'))
+            print(bytearray(b'***Reached end of file before encountering data', 'utf-8'))
             print("   Cowardly stopping here")
             print("   Some debug info:")
             print("   Have read {} lines and".format(lines))
