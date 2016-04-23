02cdda9fda8409a1fa60af2285b692d0430ecbc1
hwilson94 <hw1g13@soton.ac.uk>
2016-04-23 21:13:07 +0100

    Try changing str type to support buffer interface. Working towards compatibilty for Python 3.4 and Python 3.5.

--- ovf2vtk/Lib/omfread_new.py
+++ ovf2vtk/Lib/omfread_new.py
@@ -76,7 +76,7 @@ def what_data(filename, verbose=0):
     startline = integer (lines from beginning of file)
     type      = 'binary4','binary8','ascii'
     """
-    f = open(filename, "rt")
+    f = open(filename, "rb")
     lines = 0
     bytes = 0
     ans = {}
@@ -85,7 +85,7 @@ def what_data(filename, verbose=0):
         bytes += len(line)
         lines += 1
         if not line:               # until eof
-            print('***Reached end of file before encountering data')
+            print(bytearray('***Reached end of file before encountering data'))
             print("   Cowardly stopping here")
             print("   Some debug info:")
             print("   Have read {} lines and".format(lines))
